b0673dea12bc0df09f6c608411f45f11b969deae
suguru-3u <suguru.13579515@gmail.com>
2020-05-16 12:49:20 +0000

    [Finish] device機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_064409) do
+ActiveRecord::Schema.define(version: 2020_05_16_123334) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,19 @@ ActiveRecord::Schema.define(version: 2020_05_15_064409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "end_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
