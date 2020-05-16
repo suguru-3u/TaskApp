f54050a2e22b19662b332409ba9aa20d175df036
suguru-3u <suguru.13579515@gmail.com>
2020-05-16 14:30:55 +0000

    [Finish] todayTask

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    user_path(resource)
+    tasks_path
   end
 
   #sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :body, presence: true
+
+  belongs_to :user
 end
